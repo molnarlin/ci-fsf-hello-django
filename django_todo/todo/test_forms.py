8b57c926889af13f1106732f8542b2e21897c4ba
Katalin Molnar <molnarlin@gmail.com>
2025-01-23 16:28:14 +0000

    test added

--- django_todo/todo/test_forms.py
+++ django_todo/todo/test_forms.py
@@ -1,6 +1,18 @@
 from django.test import TestCase
+from .forms import Itemform
 
-class TestDjango(TestCase):
+class TestItemform(TestCase):
 
-    def test_this_thing_works(self):
-        self.assertEqual(1, 0)
+    def test_item_name_is_required(self):
+        form = Itemform({'name': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('name', form.error.keys())
+        self.assertEqual(form.errors['name'][0], 'This field is required.')
+
+    def test_done_field_is_not_required(self):
+        form = Itemform({'name': 'Test Todo Item'})
+        self.assertTrue(form.is_valid())
+
+    def test_fields_are_explicit_in_form_metaclass(self):
+        form = Itemform()
+        self.assertEqual(form.Meta.fields, ['name', 'done'])
