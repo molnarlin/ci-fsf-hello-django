d1df2bee16b2bfea59b1edd02e078eb3102b24ea
Katalin Molnar <molnarlin@gmail.com>
2025-01-25 20:08:07 +0000

    form tests added

--- django_todo/todo/test_forms.py
+++ django_todo/todo/test_forms.py
@@ -1,12 +1,12 @@
 from django.test import TestCase
 from .forms import Itemform
 
-class TestItemform(TestCase):
+class TestItemForm(TestCase):
 
     def test_item_name_is_required(self):
         form = Itemform({'name': ''})
         self.assertFalse(form.is_valid())
-        self.assertIn('name', form.error.keys())
+        self.assertIn('name', form.errors.keys())
         self.assertEqual(form.errors['name'][0], 'This field is required.')
 
     def test_done_field_is_not_required(self):
